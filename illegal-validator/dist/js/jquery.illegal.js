9aef49433a332b1b9575a567122d7eafa3d28959
ryvan0 <factmaker@yahoo.com>
2016-05-22 00:23:59 +0800

    dynamic form element

--- illegal-validator/dist/js/jquery.illegal.js
+++ illegal-validator/dist/js/jquery.illegal.js
@@ -7,9 +7,7 @@
   Author: Ryvan Prabhu
     Repo: http://github.com/ryvan-js/illegal-validator.git
   Issues: http://github.com/ryvan-js/illegal-validator.git/issues
-  LICENCE: MIT licence(MIT)
  */
-
 
 
 !function(){
@@ -60,6 +58,7 @@
 			keyVal = String.fromCharCode(event.keyCode || event.which).replace(/'/g, "\\'").replace(/\0/g,"");//get key values on every keypress and escapes quotes since we gonna use eval for dynamic validation for restricted input.
 			isValid = false;
 			isStrict = false; 
+			//console.log(isNaN(keyVal));
 			beforeValue = field_value.replace(/\s/g,'');//lock it under local scope
 			value = field_value.replace(/\s/g,'')  + keyVal;//lock it under local scope
 			minValid = true;
@@ -231,6 +230,10 @@
 					new_rules = [];
 					if($(elem).attr('stop-loop') !== "true"){
 
+						// if(e_rules.regexMatch(/^regex/)){
+						// 	e_rules.splice(e_rules.regexMatch(/^regex/),1);
+						// 	e_rules.log(rules);
+						// }
 						if($.map(regex_pattern,function(value,index){ return [index] }).indexOf(rule) > -1){
 							
 
@@ -311,6 +314,7 @@
 				this.label.attr('label-text',this.label.html());
 				}
 
+				//this.label.attr('label-text',this.label.html());
 			});
 
 			$(this.form).find('[rules][type]').on('focus',function(){
@@ -445,7 +449,7 @@
 			  configurable: true
 			});
 		}
-		
+		$(this).addClass('illegal-form');
 		var illegal = new Processor(this,opt);
 
 		//console.log(regex_pattern);
@@ -454,10 +458,6 @@
 		$.fn.illegal.defaults = {
 			
 		};
-		
 
 
-		
-
-
-}(jQuery);
+}();
